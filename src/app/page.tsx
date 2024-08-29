7784c317044901becf1314fff204cb5f878f9883
ahmedmsayeem <msayeemahmed76@gmail.com>
2024-08-29 22:25:54 +0530

    [feat] puzzle1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,19 @@ export default function Home() {
   return (
     <main className="p-24">
       <h2 className="text-center mb-12">Puzzle List</h2>
+
+      <p>Round1 puzzles</p>
      <ul>
-      
-      <li><Link href={"/responsive"} className="underline text-blue-500 italic">responsive puzzle</Link></li>
-      <li><Link href={""} className="underline text-blue-500 italic">add new puzzles here</Link></li> 
+
+      <li><Link href={"/responsive"} className="underline text-blue-500 italic">1. responsive puzzle</Link></li>
+      <li className="text-sm">*add new puzzles here*</li> 
         
+     </ul>
+
+     <br />
+     <p>Round2 puzzles</p>
+     <ul>
+      <li><Link href={""} className="underline text-blue-500 italic">...</Link></li>
      </ul>
     </main>
   );
