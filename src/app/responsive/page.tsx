e7cdf2b4438d6f7ef469f37f447827085e24e713
ahmedmsayeem <msayeemahmed76@gmail.com>
2024-08-29 22:25:49 +0530

    [feat] puzzle1

---
+++ src/app/responsive/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PuzzleResponsive from '@/components/puzzleResponsive/responsive'
+export default function Responive() {
+  return (
+    <div>
+
+<PuzzleResponsive></PuzzleResponsive>
+    </div>
+  )
+}
