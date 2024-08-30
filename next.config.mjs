22d7cebd1451497818bf783f3a5404b5f6134935
ahmedmsayeem <msayeemahmed76@gmail.com>
2024-08-30 15:50:27 +0530

    [feat] quizzes component added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
 
-export default nextConfig;
+/** @type {import("next").NextConfig} */
+const config = {
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.filepicker.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+
+ 
+
+};
+
+export default config;
